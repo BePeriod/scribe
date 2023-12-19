669898b163924579f50c058bea199ba7df0a42af
Shane Scanlon <shane@turtlestack.dev>
2023-12-19 12:52:10 -0500

    add simple home screen

--- backend/src/scribe/routers/pages.py
+++ backend/src/scribe/routers/pages.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -8,6 +10,7 @@ router = APIRouter()
 
 # configure the template engine
 _templates = Jinja2Templates(directory="templates")
+_templates.env.globals["now"] = datetime.datetime.utcnow()
 
 
 # define the home page route
@@ -16,33 +19,3 @@ async def home(request: Request):
     # render the template file in /templates/home.html
     # we are not currently using this request object
     return _templates.TemplateResponse("pages/home.html.j2", {"request": request})
-
-
-_todos = []
-
-
-@router.get("/todos", response_class=HTMLResponse)
-async def get_todos(request: Request):
-    return _templates.TemplateResponse(
-        "frames/todos.html.j2", {"request": request, "todos": _todos}
-    )
-
-
-@router.post("/todos", response_class=HTMLResponse)
-async def add_todo(request: Request, todo: ToDo = Depends()):
-    todo.id = len(_todos) + 1
-    _todos.append(todo)
-    return _templates.TemplateResponse(
-        "streams/add_todo.html.j2",
-        {"request": request, "todo": todo},
-        headers={"Content-Type": "text/vnd.turbo-stream.html; charset=utf-8"},
-    )
-
-
-@router.put("/todos/{todo_id}", response_class=HTMLResponse)
-async def update_todo(request: Request, todo: ToDo = Depends()):
-    global _todos
-    _todos = [todo if item.id == todo.id else item for item in _todos]
-    return _templates.TemplateResponse(
-        "frames/todo.html.j2", {"request": request, "todo": todo}
-    )
